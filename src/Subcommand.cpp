c77b275b7157fd7bdcab8fd3f6073fe4d112b41e
naoki18 <jadras.dunkan@gmail.com>
2026-02-25 09:08:35 +0100

    Better subcommand cpp

--- src/Subcommand.cpp
+++ src/Subcommand.cpp
@@ -1,45 +1,69 @@
 #include "Subcommand.h"
 #include "Context.h"
+#include <stdexcept>
 
-// Stage 3 : Subcommand
-//
-// A Subcommand has a name, a callback, and a list of accepted Options.
-// Options are stored in a vector and indexed by both their short and long name
-// in an unordered_map for fast lookup.
-// 
-// GetOption() must work with both the short name and the long name.
+/* Stage 3 : Subcommand
+
+	A Subcommand has a name, a callback, and a list of accepted Options.
+	Options are stored in a vector and indexed by both their short and long name
+	in an unordered_map.
+	This avoids to have twice the object for 2 different keys in the map.
+
+	GetOption() must work with both the short name and the long name.
+*/
 
 Subcommand::Subcommand(const std::string& name, std::function<void(const Context&)> callback)
 {
-    throw std::exception(__FUNCTION__ " not implemented");
+	/*
+	This constructor only assign parameters to members
+	*/
+    throw std::logic_error(__FUNCTION__ " not implemented");
 }
 
 void Subcommand::Exec(const Context& context)
 {
-    throw std::exception(__FUNCTION__ " not implemented");
+	/* This function execute the subcommand callback */
+    throw std::logic_error(__FUNCTION__ " not implemented");
 }
 
 const std::string& Subcommand::GetName() const
 {
-    throw std::exception(__FUNCTION__ " not implemented");
+	/*This function returns subcommand name*/
+    throw std::logic_error(__FUNCTION__ " not implemented");
 }
 
 const Option& Subcommand::GetOption(const std::string& optionName) const
 {
-    throw std::exception(__FUNCTION__ " not implemented");
+	/*
+		This function should work with short option name and long option name
+	*/
+    throw std::logic_error(__FUNCTION__ " not implemented");
 }
 
 void Subcommand::AddOption(const Option& option)
 {
-    throw std::exception(__FUNCTION__ " not implemented");
+	/*
+		This function add option with both short and long name.
+		Use the vector to store the objects, use the map to reference them.
+	*/
+    throw std::logic_error(__FUNCTION__ " not implemented");
 }
 
 void Subcommand::AddOption(const std::string& shortName, const std::string& longName, const int arity)
 {
-    throw std::exception(__FUNCTION__ " not implemented");
+	/*
+		Create and add option with both short and long name.
+		Use the vector to store the objects, use the map to reference them.
+		
+		Tip: you may look at emplace_back method of vector.
+	*/
+    throw std::logic_error(__FUNCTION__ " not implemented");
 }
 
 bool Subcommand::HasRegisteredOption(const std::string& name) const
 {
-    throw std::exception(__FUNCTION__ " not implemented");
+	/*
+		This function check if the option is registered ( both with short and long name ).
+	*/
+    throw std::logic_error(__FUNCTION__ " not implemented");
 }
